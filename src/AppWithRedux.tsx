32eec7ffad3c3b1ffd7fc00d7e718ad1b9e585e1
AnaKhurs <ana.khurs@gmail.com>
2021-12-15 22:51:05 +0300

    Update AppWithRedux.tsx

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,7 +1,6 @@
-import React, {useReducer, useState} from 'react';
+import React from 'react';
 import './App.css';
 import {TasksType, Todolist} from "./Todolist";
-import {v1} from "uuid";
 import {AddItemForm} from "./components/AddItemForm";
 import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@material-ui/core";
 import {Menu} from "@material-ui/icons";
@@ -10,7 +9,6 @@ import {
     ChangeTodolistFilterAC,
     ChangeTodolistTitleAC,
     RemoveTodoListAC,
-    todolistsReducer
 } from "./store/todolists-reducer";
 import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from "./store/tasks-reducer";
 import {useDispatch, useSelector} from "react-redux";
