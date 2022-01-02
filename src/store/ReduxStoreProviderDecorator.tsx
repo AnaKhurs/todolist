2cf165088ef1546a062269735cd30269ed600821
AnaKhurs <ana.khurs@gmail.com>
2022-01-02 19:15:54 +0300

    Update ReduxStoreProviderDecorator.tsx

--- src/store/ReduxStoreProviderDecorator.tsx
+++ src/store/ReduxStoreProviderDecorator.tsx
@@ -1,6 +1,36 @@
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {combineReducers, createStore} from "redux";
+import {tasksReducer} from "./tasks-reducer";
+import {todolistsReducer} from "./todolists-reducer";
+import {v1} from "uuid";
+
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        {id: "todolistId1", title: "What to learn", filter: "all"},
+        {id: "todolistId2", title: "What to buy", filter: "all"}
+    ],
+    tasks: {
+        ["todolistId1"]: [
+            {id: '1', title: "HTML&CSS", isDone: true},
+            {id: '2', title: "JS", isDone: true}
+        ],
+        ["todolistId2"]: [
+            {id: '11', title: "Milk", isDone: true},
+            {id: '22', title: "React Book", isDone: true}
+        ]
+    }
+};
+
+export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootStateType);
+
 
 export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
-return <Provider store={store}>{storyFn()}</Provider>
+    return <Provider store={storyBookStore}>{storyFn()}</Provider>
 }
