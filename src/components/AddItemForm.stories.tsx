49bff7fe282ef86f5e6c690b225db394b06bc643
AnaKhurs <ana.khurs@gmail.com>
2022-01-02 18:53:30 +0300

    add Task.stories

--- src/components/AddItemForm.stories.tsx
+++ src/components/AddItemForm.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import {AddItemForm} from "../components/AddItemForm";
+import {AddItemForm} from "./AddItemForm";
 import {action} from "@storybook/addon-actions";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
